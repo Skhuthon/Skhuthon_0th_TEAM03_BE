50ca9cb8fb87ae5b79f7a049bfec6eecc85429e0
younha00 <ayeon0@office.skhu.ac.kr>
2024-06-27 07:16:00 +0900

    ✨ [#2]Feat: Logging 미들웨어

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/middleware/log-middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction } from 'express';
+
+@Injectable()
+export class LogMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(
+      `[REQ] ${req.url} - ${req.method} - ${new Date().toLocaleString('kr')}`,
+    );
+    next();
+  }
+}

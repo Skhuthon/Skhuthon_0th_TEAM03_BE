5856e7ae7228aef97b369efe3ea4f06bac07d4b6
younha00 <ayeon0@office.skhu.ac.kr>
2024-06-27 06:33:07 +0900

    ✏️ [#1]Fix: 스웨거 제목 수정

--- src/util/swagger.ts
+++ src/util/swagger.ts
@@ -8,8 +8,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
  */
 export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
-    .setTitle('NestJS Study API Docs')
-    .setDescription('NestJS Study API description')
+    .setTitle('스쿠톤 3팀 API Docs')
+    .setDescription('Skhuthon Team03 API description')
     .setVersion('1.0.0')
     .build();
 

8aecf6a70cf528b6303d7a0b8701209b4ebd004d
younha00 <ayeon0@office.skhu.ac.kr>
2024-06-27 10:29:06 +0900

    [#2]Feat: 유저 엔티티에 validation 추가

--- src/users/entity/users.entity.ts
+++ src/users/entity/users.entity.ts
@@ -1,11 +1,31 @@
 import { Column, Entity } from 'typeorm';
 import { BaseModel } from '../../common/entity/base.entity';
+import { IsEmail, IsString, Length } from 'class-validator';
+import { stringValidationMessage } from '../../common/validation-message/string-validation-message';
+import { emailValidationMessgae } from '../../common/validation-message/email-validation-message';
+import { lengthValidationMessage } from '../../common/validation-message/length-validation-message';
 
 @Entity('users')
 export class UsersModel extends BaseModel {
-  @Column()
-  nickname: string;
-
-  @Column()
+  @Column({
+    unique: true,
+  })
+  @IsEmail(
+    {},
+    {
+      message: emailValidationMessgae,
+    },
+  )
   email: string;
+
+  @Column({
+    unique: true,
+  })
+  @IsString({
+    message: stringValidationMessage,
+  })
+  @Length(2, 20, {
+    message: lengthValidationMessage,
+  })
+  nickname: string;
 }

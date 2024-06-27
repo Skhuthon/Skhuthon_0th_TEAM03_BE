efb433ac47ad5577df63845cb43de5ba20899d87
younha00 <ayeon0@office.skhu.ac.kr>
2024-06-27 17:53:42 +0900

    ➖ [#2]Del: 네이버 로그인 코드 삭제

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-
 import { Body, Controller, Post, Headers, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { BasicTokenGuard } from './guard/basic-token.guard';
-import { AccessTokenGuard, RefreshTokenGuard } from './guard/bearer-token.guard';
+import { RefreshTokenGuard } from './guard/bearer-token.guard';
 import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
@@ -11,40 +10,34 @@ export class AuthController {
 
   @Post('token/access')
   @UseGuards(RefreshTokenGuard)
-  postTokenAccess(
-    @Headers('authorization') rawToken: string,
-  ){
+  postTokenAccess(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, true);
-    const newToken = this.authService.rotateToken(token, false,);
+    const newToken = this.authService.rotateToken(token, false);
     /**
      * {accessToken: {token}}
      */
     return {
       accessToken: newToken,
-    }
+    };
   }
 
   @Post('token/refresh')
   @UseGuards(RefreshTokenGuard)
-  postTokenRefresh(
-    @Headers('authorization') rawToken: string,
-  ){
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, true);
-    const newToken = this.authService.rotateToken(token, true,);
+    const newToken = this.authService.rotateToken(token, true);
     /**
      * {refreshToken: {token}}
      */
     return {
       refreshToken: newToken,
-    }
+    };
   }
 
   // 로그인
   @Post('login/email')
   @UseGuards(BasicTokenGuard)
-  postLoginEmail(
-    @Headers('authorization') rawToken: string,
-  ) {
+  postLoginEmail(@Headers('authorization') rawToken: string) {
     const token = this.authService.extractTokenFromHeader(rawToken, false);
 
     const credentials = this.authService.decodeBasicToken(token);
@@ -53,11 +46,7 @@ export class AuthController {
 
   // 회원가입
   @Post('register/email')
-  postRegisterEmail(
-    @Body() body: RegisterUserDto,
-  ) {
-    return this.authService.registerWithEmail(
-      body,
-    );
+  postRegisterEmail(@Body() body: RegisterUserDto) {
+    return this.authService.registerWithEmail(body);
   }
 }

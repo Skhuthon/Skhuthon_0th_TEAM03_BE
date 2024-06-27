1ea2d81ae80035045f58c29e8f804136374e24c5
younha00 <ayeon0@office.skhu.ac.kr>
2024-06-27 19:41:02 +0900

    ✨ [#3]Feat: 카카오 로그인

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,50 @@
-import { Body, Controller, Post, Headers, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Headers,
+  UseGuards,
+  Get,
+  Res,
+  Header, Query,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { BasicTokenGuard } from './guard/basic-token.guard';
 import { RefreshTokenGuard } from './guard/bearer-token.guard';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { ConfigService } from '@nestjs/config';
+import {
+  ENV_KAKAO_CLIENT_ID_KEY,
+  ENV_KAKAO_REDIRECT_URL_KEY,
+} from '../common/const/env-keys.const';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('인증 관련 API')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @ApiOperation({ summary: '카카오 로그인' })
+  @Get('kakao-login-page')
+  @Header('Content-Type', 'text/html')
+  async kakaoRedirect(@Res() res: Response): Promise<void> {
+    const client_id = this.configService.get<string>(ENV_KAKAO_CLIENT_ID_KEY);
+    const redirect_uri = this.configService.get<string>(
+      ENV_KAKAO_REDIRECT_URL_KEY,
+    );
+    const url = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
+    res.redirect(url);
+  }
+
+  @ApiOperation({ summary: '카카오 로그인 리다이렉트' })
+  @Get('kakao/redirect')
+  async getKakaoInfo(@Query() code: string) {
+    console.log(code);
+  }
 
   @Post('token/access')
   @UseGuards(RefreshTokenGuard)

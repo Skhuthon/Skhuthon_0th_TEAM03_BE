af0b7b6dbd28e10cefcfbb78116b81f5daa33589
younha00 <ayeon0@office.skhu.ac.kr>
2024-06-29 16:12:20 +0900

    Fix: 리다이렉트 uri 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
 
   loginUser(user: Pick<UsersModel, 'email' | 'id'>, res: Response) {
     const accessToken = this.signToken(user);
-    res.redirect(`http://localhost:3000/?accessToken=${accessToken}`);
+    res.redirect(`http://localhost:3000/auth/kakao/redirect?accessToken=${accessToken}`);
   }
 
   async kakaoLogin(
